ac76640db2580a680043086b0806f98504c94c9a
Haoxi Tan <haoxi.tan@gmail.com>
2023-02-12 09:19:06 +1000

    fixing ipv4 hashing using useless bits

--- main.go
+++ main.go
@@ -48,8 +48,8 @@ func IPtoHumanDNS(ipstr string) string {
 
 		return result6 + ".ip6"
 	} else {
-		log.Printf("[IPtoHash] ipv4 parsed: %#v", parsed)
-		result4, err := humanhash.Humanize(parsed, 4)
+		log.Printf("[IPtoHash] ipv4 parsed: bit %#v", parsed[12:16])
+		result4, err := humanhash.Humanize(parsed[12:16], 4)
 		if err != nil {
 			log.Printf("%s", err)
 			return "error"
@@ -163,17 +163,9 @@ func handleRequests() {
 func main() {
 	ip4str := "192.168.1.2"
 	ip6str := "2606:4700:4700::1111"
-	ip4 := net.ParseIP(ip4str) //[]byte{192,168,1,1}
-	ip6 := net.ParseIP(ip6str)
-	log.Printf("(example) ipv4 parsed: %#v", ip4)
-	log.Printf("(example) ipv6 parsed: %#v", ip6)
 
-	result4, _ := humanhash.Humanize(ip4, 4)
-	result6_round1, _ := humanhash.Humanize(ip6, 8)
-	// double hash it so it has enough entropy
-	result6, _ := humanhash.Humanize([]byte(result6_round1), 5) // make this one 5 words so that it's distinguishable from ipv4
-	log.Printf("(example) ipv4 name for address %s = %s.ip4", ip4str, result4)
-	log.Printf("(example) ipv4 name for address %s = %s.ip6", ip6str, result6)
+	log.Printf("(example) ipv4 name for address %s = %s", ip4str, IPtoHumanDNS(ip4str))
+	log.Printf("(example) ipv4 name for address %s = %s", ip6str, IPtoHumanDNS(ip6str))
 
 	ExpiryTimeInSeconds = 1800
 
